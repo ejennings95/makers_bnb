d680a94d6e5ddd5db986980ed67a840812787481
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-11 17:28:32 +0000

    Added routes to match front end dev views

--- app.rb
+++ app.rb
@@ -6,5 +6,26 @@ require_relative './lib/database_setup'
 class Makersbnb < Sinatra::Base
     register Sinatra::Flash
 
+
+    get ('/') do
+      erb(:index)
+    end
+
+    get ('/browse')
+      erb(:property_list)
+    end
+
+    get('/browse/:id') do
+      erb(:property_details)
+    end
+
+    get ('/browse/:id/confirmation') do
+      erb(:confirmation_pending)
+    end
+
+    get ('/ownerlogin') do
+      erb(:log_in)
+    end
+
   run! if app_file == $0
 end
