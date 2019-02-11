c60c71aeeac364f0716862afea7400b3628c3679
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-11 15:02:10 +0000

    database and tables created, tests passed

--- spec/test_database.rb
+++ spec/test_database.rb
@@ -3,6 +3,6 @@ require 'pg'
 def test_database
   conn = PG.connect(dbname: 'Makersbnb_test')
   # Clean the tables
-  conn.exec("TRUNCATE ;")
+  conn.exec("TRUNCATE property_owner, properties;")
   # insert table name after truncate
 end
