f27aa0a960d266d446e7b5267242185bc4188295
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-11 17:20:58 +0000

    added dates booked to properties db and tested

--- lib/properties.rb
+++ lib/properties.rb
@@ -1,19 +1,20 @@
 class Properties
 
-  attr_reader :id, :name, :description, :price, :location, :property_owner_id
+  attr_reader :id, :name, :description, :price, :location, :property_owner_id, :dates_booked
 
-def initialize(id:, name:, description:, price:, location:, property_owner_id:)
+def initialize(id:, name:, description:, price:, location:, property_owner_id:, dates_booked:)
   @id = id
   @name = name
   @description = description
   @price = price
   @location = location
   @property_owner_id = property_owner_id
+  @dates_booked = dates_booked
 end
 
-def self.add(name:, description:, price:, location:, property_owner_id:)
-  result = Database.query( "INSERT INTO properties(name, description, price, location, property_owner_id) VALUES('#{name}','#{description}', '#{price}', '#{location}', '#{property_owner_id}') RETURNING name, description, price, location, property_owner_id;")
-  Properties.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], location: result[0]['location'], property_owner_id: result[0]['property_owner_id'])
+def self.add(name:, description:, price:, location:, property_owner_id:, dates_booked:)
+  result = Database.query( "INSERT INTO properties(name, description, price, location, property_owner_id, dates_booked) VALUES('#{name}','#{description}', '#{price}', '#{location}', '#{property_owner_id}', '#{dates_booked}') RETURNING name, description, price, location, property_owner_id, dates_booked;")
+  Properties.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], location: result[0]['location'], property_owner_id: result[0]['property_owner_id'], dates_booked: result[0]['dates_booked'])
 end
 
 end
--- spec/properties_spec.rb
+++ spec/properties_spec.rb
@@ -4,22 +4,27 @@ describe Properties do
   let(:property_owner) { double(PropertyOwner.add(name: 'name'), id: PropertyOwner.list.first.id) }
 
   it 'can be create a property and save its name' do
-    property = Properties.add(name: 'name', description: 'description', price: 10, location: 'location', property_owner_id: property_owner.id)
+    property = Properties.add(name: 'name', description: 'description', price: 10, location: 'location', property_owner_id: property_owner.id, dates_booked: '2018-04-15')
     expect(property.name).to eq 'name'
   end
 
   it 'can be create a property and save its description' do
-    property = Properties.add(name: 'name', description: 'description', price: 10, location: 'location', property_owner_id: property_owner.id)
+    property = Properties.add(name: 'name', description: 'description', price: 10, location: 'location', property_owner_id: property_owner.id, dates_booked: '2018-04-15')
     expect(property.description).to eq 'description'
   end
 
   it 'can be create a property and save its price' do
-    property = Properties.add(name: 'name', description: 'description', price: 10, location: 'location', property_owner_id: property_owner.id)
+    property = Properties.add(name: 'name', description: 'description', price: 10, location: 'location', property_owner_id: property_owner.id, dates_booked: '2018-04-15')
     expect(property.price).to eq 10.to_s
   end
 
   it 'can be create a property and save its location' do
-    property = Properties.add(name: 'name', description: 'description', price: 10, location: 'location', property_owner_id: property_owner.id)
+    property = Properties.add(name: 'name', description: 'description', price: 10, location: 'location', property_owner_id: property_owner.id, dates_booked: '2018-04-15')
     expect(property.location).to eq 'location'
+  end
+
+  it 'can be create a property and save its unavailble dates' do
+    property = Properties.add(name: 'name', description: 'description', price: 10, location: 'location', property_owner_id: property_owner.id, dates_booked: '2018-04-15')
+    expect(property.dates_booked).to eq '2018-04-15'
   end
 end
