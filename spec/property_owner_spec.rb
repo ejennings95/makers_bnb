730b1d369c20c1704f9bae68313c857508d20f0e
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-11 15:31:26 +0000

    failing test for property owner class

---
+++ spec/property_owner_spec.rb
@@ -0,0 +1,8 @@
+require 'property_owner'
+
+describe PropertyOwner do
+  it 'can be create a new property owner' do
+    owner = PropertyOwner.add(name: 'owner1')
+    expect(user.name).to eq 'owner1'
+  end
+end
