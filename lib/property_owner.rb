ce9e0a773e8d18409a6b1668456ef556e56df89c
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-12 19:57:26 +0000

    added in the login / logout functionality and used bcrypt

--- lib/property_owner.rb
+++ lib/property_owner.rb
@@ -1,3 +1,5 @@
+require 'bcrypt'
+
 class PropertyOwner
   attr_reader :id, :name, :username, :email, :password
 
@@ -16,7 +18,19 @@ class PropertyOwner
 end
 
   def self.add(name:, username:, email:, password:)
-    result = Database.query( "INSERT INTO property_owner(name, username, email, password) VALUES('#{name}', '#{username}', '#{email}', '#{password}') RETURNING id, name, username, email, password;")
+    if (Database.query("SELECT * FROM property_owner WHERE email = '#{email}'") || Database.query("SELECT * FROM property_owner WHERE username = '#{username}'")).any?
+      return
+    else
+      encrypted_password = BCrypt::Password.create(password)
+      result = Database.query( "INSERT INTO property_owner(name, username, email, password) VALUES('#{name}', '#{username}', '#{email}', '#{encrypted_password }') RETURNING id, name, username, email, password;")
+      PropertyOwner.new(id: result[0]['id'], name: result[0]['name'], username: result[0]['username'], email: result[0]['email'], password: result[0]['password'])
+    end
+  end
+
+  def self.login(email:, password:)
+    result = Database.query( "SELECT * FROM property_owner WHERE email = '#{email}'")
+    return unless result.any?
+    return unless BCrypt::Password.new(result[0]['password']) == password
     PropertyOwner.new(id: result[0]['id'], name: result[0]['name'], username: result[0]['username'], email: result[0]['email'], password: result[0]['password'])
   end
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,5 @@
+require 'bcrypt'
+
 class User
   attr_reader :id, :name, :username, :email, :password
 
@@ -12,11 +14,23 @@ class User
   def self.list
   Database.query( "SELECT * FROM users" ).map do | row |
     User.new(id: row['id'], name: row['name'], username: row['username'], email: row['email'], password: row['password'])
+    end
   end
-end
 
   def self.add(name:, username:, email:, password:)
-    result = Database.query( "INSERT INTO users(name, username, email, password) VALUES('#{name}', '#{username}', '#{email}', '#{password}') RETURNING id, name, username, email, password;")
+    if (Database.query("SELECT * FROM users WHERE email = '#{email}'") || Database.query("SELECT * FROM users WHERE username = '#{username}'")).any?
+      return
+    else
+      encrypted_password = BCrypt::Password.create(password)
+      result = Database.query( "INSERT INTO users(name, username, email, password) VALUES('#{name}', '#{username}', '#{email}', '#{encrypted_password}') RETURNING id, name, username, email, password;")
+      User.new(id: result[0]['id'], name: result[0]['name'], username: result[0]['username'], email: result[0]['email'], password: result[0]['password'])
+    end
+  end
+
+  def self.login(email:, password:)
+    result = Database.query( "SELECT * FROM users WHERE email = '#{email}'")
+    return unless result.any?
+    return unless BCrypt::Password.new(result[0]['password']) == password
     User.new(id: result[0]['id'], name: result[0]['name'], username: result[0]['username'], email: result[0]['email'], password: result[0]['password'])
   end
 end
